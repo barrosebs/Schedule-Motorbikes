e154a268fc3c31f1305b3557c3e46e9af4a0aba6
Eduardo Barros Dos Santos <barrosebs@gmail.com>
2024-05-08 21:45:08 -0300

    Adding Context for Bike and Deliver Person Setting Fields to Unique

--- SM.Data/Context/SMContext.cs
+++ SM.Data/Context/SMContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SM.Domain.Model;
 using SM.Domain.Models;
 
 namespace SM.Data.Context
@@ -12,11 +13,24 @@ namespace SM.Data.Context
         {
         }
 
+        public DbSet<Bike> Bikes { get; set; }
+        public DbSet<DeliveryPerson> DeliveryPeople { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
-
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Bike>()
+            .HasIndex(p => p.LicenseTag)
+            .IsUnique();
+
+            modelBuilder.Entity<DeliveryPerson>()
+            .HasIndex(p => p.NumberCNPJ)
+            .IsUnique();
+
+            modelBuilder.Entity<DeliveryPerson>()
+            .HasIndex(p => p.NumberCNH)
+            .IsUnique();
         }
     }
 }
