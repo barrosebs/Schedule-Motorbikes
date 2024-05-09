d1b1c73e999e4b6a5a575ba72366c482893ab49b
Eduardo Barros Dos Santos <barrosebs@gmail.com>
2024-05-08 21:36:50 -0300

    add objet DeliveryPerson

---
+++ SM.Domain/Model/DeliveryPerson.cs
@@ -0,0 +1,17 @@
+﻿using SM.Domain.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SM.Domain.Model
+{
+    public class DeliveryPerson : BasicModel
+    {
+        [MaxLength(120)]
+        public required string FullName { get; set; }
+        [Column(TypeName = "char(14)")]
+        public required string NumberCNPJ { get; set; }
+        public required string NumberCNH { get; set; }
+        public ETypeCNH TypeCNH { get; set; }
+        public string UrlImageCNH { get; set; }
+    }
+}
