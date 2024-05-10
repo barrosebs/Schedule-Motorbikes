06156e0c083a5b6984a7f85a326dc083ca0803f2
Eduardo Barros Dos Santos <barrosebs@gmail.com>
2024-05-10 20:43:53 -0300

    add accountControler

--- SM.Web/Controllers/HomeController.cs
+++ SM.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 namespace SW.Web.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Home")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -17,6 +17,7 @@ namespace SW.Web.Controllers
         [HttpGet("~/Index")]
         public IActionResult Index()
         {
+            ViewData["Title"] = "Página Inicial";
             return View();
         }
         [HttpGet("~/Privacy")]
