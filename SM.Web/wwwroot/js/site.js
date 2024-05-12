f811c4ec7e9b40adac342ee05669172363eae6b1
Eduardo Barros Dos Santos <barrosebs@gmail.com>
2024-05-12 01:14:17 -0300

    mask input

--- SM.Web/wwwroot/js/site.js
+++ SM.Web/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function maskCNPJ(input) {
+    const CPFNumber = input.value.replace(/\D/g, '');
+
+    let formattedCPFNumber = '';
+    for (let i = 0; i < CPFNumber.length; i++) {
+        if (i === 2) {
+            formattedCPFNumber += '.';
+        } else if (i === 5) {
+            formattedCPFNumber += '.';
+        } else if (i === 8) {
+            formattedCPFNumber += '/';
+        } else if (i === 12) {
+            formattedCPFNumber += '-';
+        }
+        formattedCPFNumber += CPFNumber[i];
+    }
+
+    input.value = formattedCPFNumber;
+}
+
+function capitalizeWords(str) {
+    return str.replace(/\b\w/g, function (char) {
+        return char.toUpperCase();
+    });
+}
