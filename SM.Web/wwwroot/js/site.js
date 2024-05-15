9aaa0d0c6ed206a6c90ba84410fdbc4aeadffd78
Eduardo Barros Dos Santos <barrosebs@gmail.com>
2024-05-15 18:56:41 -0300

    Adding and removing CNPJ field masks.

--- SM.Web/wwwroot/js/site.js
+++ SM.Web/wwwroot/js/site.js
@@ -2,7 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    var cnpjElements = document.querySelectorAll(".cnpj-mask");
+    cnpjElements.forEach(function (element) {
+        var cnpj = element.textContent;
+        element.textContent = formatarCNPJ(cnpj);
+    });
+});
 
+function formatarCNPJ(cnpj) {
+    // Remove caracteres indesejados
+    cnpj = cnpj.replace(/[^\d]/g, "");
+
+    // Aplica a máscara
+    return cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
+}
 function maskCNPJ(input) {
     const CPFNumber = input.value.replace(/\D/g, '');
 
@@ -22,7 +36,10 @@ function maskCNPJ(input) {
 
     input.value = formattedCPFNumber;
 }
-
+function IsNumber(input) {
+    const IsNumber = input.value.replace(/\D/g, '');
+    input.value = IsNumber;
+}
 function capitalizeWords(str) {
     return str.replace(/\b\w/g, function (char) {
         return char.toUpperCase();
